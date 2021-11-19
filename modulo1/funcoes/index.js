719c8b0f5a18131d01902d3be476a1bd64fde9e8
Laine Moura <lainermoura@gmail.com>
2021-11-19 12:41:47 -0300

    resolucao dos exercicios de interpretacao e do exercicio 1 de escrita de codigo

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,34 @@
+/* EXERCÍCIOS DE INTEREPRETAÇÃO DE CÓDIGO 
+1-
+a. 10
+   50
+
+   b. É o mesmo que chamar a função. Não mostraria o resultado no console.
+*/
+
+/*
+ 2-
+ a. Guarda o valor inserido pelo usuário na varável *textoDoUsuario*. 
+ Cria uma função que retorna o texto em minúsculo e se contém a palavra 'cenoura' no texto digitado.
+ Guarda o resultado da função na variável *resposta* e pede pra imprimir.
+ 
+ b. i.true
+    ii.true
+    iii.true
+
+ */
+
+/* EXERCÍCIOS DE ESCRITA DE CÓDIGO */
+// 1
+//a
+/*function dadosUsuarioTeste() {
+    console.log(`Eu sou Laíne, tenho 30 anos, moro no Rio de Janeiro e sou geógrafa.`);
+} 
+dadosUsuarioTeste()*/
+
+/*function dadosUsuario(nome, idade, endereco, profissao) {
+    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro no ${endereco} e sou ${profissao}.`);
+} 
+dadosUsuario('Laíne', 30, 'Rio de Janeiro', 'geógrafa')*/
+
+
