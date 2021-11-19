925a1925b8305afff50867dbef3edd151d7f93c0
Laine Moura <lainermoura@gmail.com>
2021-11-19 13:06:47 -0300

    resolucao exercicio 2 - escrita de codigo

--- modulo1/funcoes/index.js
+++ modulo1/funcoes/index.js
@@ -1,34 +1,69 @@
-/* EXERCÍCIOS DE INTEREPRETAÇÃO DE CÓDIGO 
-1-
-a. 10
-   50
+/*EXERCÍCIOS DE INTEREPRETAÇÃO DE CÓDIGO*/
+//1-
+//a. 
 
-   b. É o mesmo que chamar a função. Não mostraria o resultado no console.
-*/
+// 10
+// 50
 
-/*
- 2-
- a. Guarda o valor inserido pelo usuário na varável *textoDoUsuario*. 
- Cria uma função que retorna o texto em minúsculo e se contém a palavra 'cenoura' no texto digitado.
- Guarda o resultado da função na variável *resposta* e pede pra imprimir.
+//b.
+//É o mesmo que chamar a função. Não mostraria o resultado no console.
+
+
+
+ //2-
+
+//  a. Guarda o valor inserido pelo usuário na varável *textoDoUsuario*. 
+//  Cria uma função que retorna o texto em minúsculo e se contém a palavra 'cenoura' no texto digitado.
+//  Guarda o resultado da função na variável *resposta* e pede pra imprimir.
  
- b. i.true
-    ii.true
-    iii.true
+//  b. i.true
+//     ii.true
+//     iii.true
 
- */
+ 
 
 /* EXERCÍCIOS DE ESCRITA DE CÓDIGO */
 // 1
 //a
-/*function dadosUsuarioTeste() {
-    console.log(`Eu sou Laíne, tenho 30 anos, moro no Rio de Janeiro e sou geógrafa.`);
-} 
-dadosUsuarioTeste()*/
 
-/*function dadosUsuario(nome, idade, endereco, profissao) {
-    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro no ${endereco} e sou ${profissao}.`);
-} 
-dadosUsuario('Laíne', 30, 'Rio de Janeiro', 'geógrafa')*/
+// function dadosUsuarioTeste() {
+//     console.log(`Eu sou Laíne, tenho 30 anos, moro no Rio de Janeiro e sou geógrafa.`);
+// } 
+// dadosUsuarioTeste()
 
+//b.
+
+// function dadosUsuario(nome, idade, endereco, profissao) {
+//     console.log(`Eu sou ${nome}, tenho ${idade} anos, moro no ${endereco} e sou ${profissao}.`);
+// } 
+// dadosUsuario('Laíne', 30, 'Rio de Janeiro', 'geógrafa')
+
+
+//2
+//a.
+
+// function soma(n1, n2) {
+//     console.log(n1+n2);
+// } soma(5, 3)
+
+//b.
+
+// function oNumeroEIgual(n1, n2) {
+//     console.log(n1>=n2);
+// } 
+// oNumeroEIgual(8,5)
+
+//c.
+
+// function ePar(n1) {
+//     console.log(n1%2 === 0);
+// } 
+// ePar(5)
+
+//d.
+
+// function tamanhoString(string) {
+//     console.log(string.length);
+// }
+// tamanhoString(`A vida é bela!`)
 
