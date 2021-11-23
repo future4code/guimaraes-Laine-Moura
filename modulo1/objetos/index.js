2d4076a3637ba1e085e99112ef77a55824db9c08
Laine Moura <lainermoura@gmail.com>
2021-11-23 15:36:49 -0300

    resolucao ex 1 escrita de codigo

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -21,3 +21,36 @@ undefined
 b. Foi chamadado 'backender' e ele tem um valor falso, por isso retornou falso. E a propriedade altura não foi definida, 
 sem atribuição de valor.
 */
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+/*
+//1
+//a.
+let animaisFofinhos = {
+    nome: 'Pooh',
+    apelidos: ['ursinho', 'fofinho', 'amarelinho']};
+
+function bichinhos() {
+    return animaisFofinhos;
+
+}console.log(`Esse é o ursinho: ${animaisFofinhos.nome}. Também é chamado de: ${animaisFofinhos.apelidos}.`);
+
+ //b.
+  let animaisFofinhos = {
+    nome: 'Pooh',
+    apelidos: ['ursinho', 'fofinho', 'amarelinho']};
+
+function bichinhos() {
+    return animaisFofinhos;
+
+}
+
+let novosAnimais = {
+    ...animaisFofinhos,
+    apelidos: ['pequeno', 'pelúcia', 'bichinho']
+}
+console.log(`Esse é o ursinho: ${novosAnimais.nome}. Também é chamado de: ${novosAnimais.apelidos}.`);
+ 
+ */
+
+
