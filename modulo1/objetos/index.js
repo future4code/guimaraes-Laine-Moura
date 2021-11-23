4f8f58c48ff05bfa6fb156657877800a3063ff68
Laine Moura <lainermoura@gmail.com>
2021-11-23 14:59:31 -0300

    resolucao exercicios de interepretacao de codigo

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,21 @@
+// EXERCÍCIOS DE INTEPRETAÇÃO DE CÓDIGO
+/*
+//1-
+//a.
+Matheus Nachtergaele
+Virginia Cavendish
+{ canal: 'Globo', horario: '14h' }
+
+//2
+//a.
+{idade: 3, nome: "Juca", raca: "SRD"}
+{idade: 3, nome: "Juba", raca: "SRD"}
+{idade: 3, nome: "Jubo", raca: "SRD"}
+
+b. Faz cópia de um objeto ou array inteiro
+
+//3
+//a.
+false
+undefined
+*/
