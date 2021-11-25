85b5fbe0ab4947813d616eb5a4a4aaa1927cb30a
Laine Moura <lainermoura@gmail.com>
2021-11-25 11:01:07 -0300

    resolucao dos exercicios de escrita de código

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -16,3 +16,53 @@ b.  10 > 0: "Esse numero passou no teste" / - 10 < 0: não aparece nada
 c. a variável mensagem aparece como não definida pois o código está chamando no escopo global e ela está no local
 */
 
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+//1
+let idade = Number(prompt('Qual é a sua idade?'));
+
+if (idade >= 18) {
+    console.log('Você pode dirigir');
+} else {
+    console.log('Você não pode dirigir');
+}
+
+//2
+let turno = prompt('Em qual turno você estuda? Digite: M (matutino) ou V (Vespertino) ou N (Noturno)').toUpperCase();
+
+if (turno === "M") {
+    console.log('Bom dia!');
+} else if (turno === 'V') {
+    console.log('Boa tarde!');
+} else if (turno === 'N') {
+    console.log('Boa noite!');
+} else {
+    console.log('Digite uma letra válida!');
+}
+
+//3
+let turnoDoAluno = prompt('Em qual turno você estuda? Digite: M (matutino) ou V (Vespertino) ou N (Noturno)').toUpperCase();
+
+switch (turnoDoAluno) {
+    case "M":
+        console.log('Bom dia!')
+        break;
+    case "V":
+        console.log('Boa tarde!')
+        break;
+    case "N":
+        console.log('Boa noite!')
+        break;
+    default:
+        console.log('Digite uma letra válida!');
+}
+
+//4
+let generoFilme = prompt('Qual o gênero do filme?').toLowerCase();
+let valorIngresso = Number(prompt('Qual o valor do ingresso?'));
+
+if (generoFilme === 'fantasia' && valorIngresso < 15) {
+    console.log('Bom filme!!!');
+} else {
+    console.log('Escolha outro filme!');
+}
+
