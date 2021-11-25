2f1587ee6b4d1f248edfbb1e6f2b7aaf28dad0a9
Laine Moura <lainermoura@gmail.com>
2021-11-25 10:29:23 -0300

    resolucao exercicios de interpretacao de codigo

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,18 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+/* 
+1.
+a. verifica se o número é par
+b. números pares
+c. números ímpares
+
+2.
+a. para informar o valor da fruta digitada,
+b. se a fruta for maça será impresso no console o valor de R$ 2.25
+c. Iria imprimir o próximo preço, onde há o break 
+
+3.
+a. guardando o número digitado pelo usuário na variável numero
+b.  10 > 0: "Esse numero passou no teste" / - 10 < 0: não aparece nada
+c. a variável mensagem aparece como não definida pois o código está chamando no escopo global e ela está no local
+*/
+
