4c09d22d8d6ac7f37a62a64f969923f492cb4c50
Laine Moura <lainermoura@gmail.com>
2021-11-30 19:08:20 -0300

    altera resposta do exercicio 1 - escrita de codigo

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -1,20 +1,27 @@
 //Exercícios de interpretação de código
 /*
 1. Declara a variável valor começando com 0, percorrendo de 0 a 4 e fazendo a soma dos valores cada vez que passa pela condição
-*/
+
 let valor = 0
 for(let i = 0; i < 5; i++) {
-  //valor = 0 + 0 // soma 0
-  //valor = 0 + 1 // soma 1
-  //valor = 1 + 2 // soma 2
-  //valor = 3 + 3 // soma 3
-  //valor =  6 + 4 // soma 4 // encerra com o índice 4
-  //valor = 10
-  valor+=i
+  valor += i
+
+  fazer iteração enquanto índice < 5
+começa em 0 // índice 0
+0 = 0 + 0
+passa na condição // índice 1
+0 = 0 + 1
+passa na condição // índice 2
+1 = 1 + 2
+passa na condição // índice 3
+3 = 3 + 3
+passa na condição // índice 4
+10 = 6 + 4
+agora meu índice é maior do que 5, encerra a condição e assim a iteração
 }
 console.log(valor)
-/*
-2// 
+
+//2 
 a. Imprime todos os números maiores que 18
 b. Com esse método não é possível acessar o índice
 
