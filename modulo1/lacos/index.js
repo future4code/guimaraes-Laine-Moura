dda0b8443fa8a2e40d74181068331454be661414
Laine Moura <lainermoura@gmail.com>
2021-11-30 18:20:52 -0300

    altera ex 2 b interpretacao de codigo

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -15,11 +15,7 @@ console.log(valor)
 
 2// 
 a. Imprime todos os números maiores que 18
-b. 
-const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
- for (let numero of lista) {
-		console.log(numero)
-	}
+b. Com esse método não é possível acessar o índice
 
 3.
 *
