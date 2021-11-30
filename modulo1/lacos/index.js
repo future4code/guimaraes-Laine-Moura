c36fd0c15171a1449360775167d8a77c1ad24c75
Laine Moura <lainermoura@gmail.com>
2021-11-30 12:40:55 -0300

    resolucao dos exercicios de interpretacao

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,30 @@
+//Exercícios de interpretação de código
+/*
+1. Declara a variável valor começando com 0, percorrendo de 0 a 4 e fazendo a soma dos valores cada vez que passa pela condição
+
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  //valor = 0 + 0 // soma 0
+  //valor = 0 + 1 // soma 1
+  //valor = 1 + 2 // soma 2
+  //valor = 3 + 3 // soma 3
+  //valor =  6 + 4 // soma 4 // encerra com o índice 4
+  //valor = 10
+}
+console.log(valor)
+
+2// 
+a. Imprime todos os números maiores que 18
+b. 
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+ for (let numero of lista) {
+		console.log(numero)
+	}
+
+3.
+*
+**
+***
+****
+
+*/
