ba955bf83e1d6e71309feb2c2d4acc3a97291459
Laine Moura <lainermoura@gmail.com>
2021-12-02 11:21:43 -0300

    resolucao dos exercicios de intepretacao de codigo

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,11 @@
+// Exercício de interepretação de código
+/*
+//1
+Cria uma nova array com o resultado da chamada de uma função para cada elemento da array
+
+//2
+Imprimirá um array com os nomes
+
+//3
+Imprimirá os arrays que não contém o valor 'Chijo'
+*/
