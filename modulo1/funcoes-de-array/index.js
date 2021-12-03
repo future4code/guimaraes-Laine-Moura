7e5ad7505f6b4e5b7fb4c830a9892a4cedb1f828
Laine Moura <lainermoura@gmail.com>
2021-12-02 21:49:45 -0300

    resolucao exerc 2 de escrita de codigo

--- modulo1/funcoes-de-array/index.js
+++ modulo1/funcoes-de-array/index.js
@@ -43,23 +43,23 @@ const returnNomeCachorro = pets.map((item, index, array) => {
     return item.nome
 })
 
-console.log(returnNomeCachorro)
+//console.log(returnNomeCachorro)
 
 // b - cria um novo array apenas com os cachorros salsicha
 
-const returnCachorroSalsicha = pets.filter((item, index, array) => {
+const returnCachorroSalsicha = pets.filter((item) => {
     return item.raca === "Salsicha"
 })
 
-console.log(returnCachorroSalsicha)
+//console.log(returnCachorroSalsicha)
 
 // c - Cria um novo array que possuirá mensagens para enviar para todos os clientes que são poodles. A mensagem deve ser: "Você ganhou um cupom de desconto de 10% para tosar o/a [NOME]!"
 
 
 const returnPoodlePromo = pets.filter((item) => {
-    if (item.raca === "Poodle")
-    return item.nome
-        
+   if (item.raca === "Poodle")
+      return item.nome
+
 })
 
 const mensagem = returnPoodlePromo.map((item) => {
@@ -67,6 +67,49 @@ const mensagem = returnPoodlePromo.map((item) => {
 })
 
 
-console.log(mensagem);
+//console.log(mensagem);
 
+//2
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+//console.log(produtos);
+
+// a - Cria um novo array que contém apenas o nome de cada item
+ const nomeProdutos = produtos.map((item) => {
+    return item.nome
+})
+//console.log(nomeProdutos)
+
+
+// b -  incompleto - Cria um novo array que contém um objeto com o nome e o preço de cada item, aplicando 5% de desconto em todos eles 
+const produtosComDEsconto = produtos.map((item) => {
+    const novoItem = {};
+    novoItem.preco = (item.preco * 0.95).toFixed(2);
+    novoItem.nome = item.nome;
+
+    return  {
+        nome: item.nome,
+        preco: (item.preco*0.95).toFixed(2)
+    }
+})
+
+console.log(produtosComDEsconto);
+
+
+// c - Crie um novo array que contenha apenas os objetos da categoria Bebidas
+const arrBebidas = produtos.filter((item) => {
+    return item.categoria === "Bebidas"
+})
+//console.log(arrBebidas);
 
