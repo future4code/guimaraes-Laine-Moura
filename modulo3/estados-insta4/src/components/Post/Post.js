86b454331f872e3fe29f42e6d06d87bb7b7e5b7a
Laine Moura <lainermoura@gmail.com>
2022-02-02 08:46:36 -0300

    ajusta contador para descurtir postagem

--- modulo3/estados-insta4/src/components/Post/Post.js
+++ modulo3/estados-insta4/src/components/Post/Post.js
@@ -49,11 +49,17 @@ class Post extends React.Component {
   }
 
   onClickCurtida = () => {
-    console.log('Curtiu!')
-    this.setState({
-      curtido: !this.state.curtido,
-      numeroCurtidas: this.state.numeroCurtidas + 1
-    })
+    if (this.state.curtido){
+      this.setState({
+        curtido: false,
+        numeroCurtidas: this.state.numeroCurtidas - 1
+      })
+    } else {
+      this.setState({ 
+        curtido: true,
+        numeroCurtidas: this.state.numeroCurtidas +1
+      })
+    }
   }
 
   onClickComentario = () => {
