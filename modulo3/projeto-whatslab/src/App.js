5832e10e7c2dba75d055872e9b58856394ad8c9e
Laine Moura <lainermoura@gmail.com>
2022-02-03 19:33:21 -0300

    cria funcoes do onChange

--- modulo3/projeto-whatslab/src/App.js
+++ modulo3/projeto-whatslab/src/App.js
@@ -1,16 +1,61 @@
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
+const Body = styled.div`
+margin: 0;
+padding: 0;
+background-color: #282c34;
+min-height: 100vh;
+color: white;
+`
+
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 3fr 6fr 1fr;
+  position: fixed;
+  bottom: 0;
+`
+
+const Input = styled.input`
+  font-family: Arial;
+`
+
+const Button = styled.button`
+  color: #282c34;
+`
+
+export class Whatslab extends React.Component {
+
+  state ={
+    valorUser: '',
+    valorMsg: '',
+  }
+
+  onChangeUser = (e) => {
+    this.setState({valorUser: e.target.value})
+  }
+
+  onChangeMsg = (e) => {
+    this.setState({valorMsg: e.target.value})
+  }
+
+  onClickBotao = () => {
+    console.log('você clicou em enviar');
+  }
+
+render() {
   return (
-    <div className='body'>
-      <form className='form-class'>
-        <input type='text' className='user' placeholder='Usuário'/>
-        <input type='text' className='msg' placeholder='Digite aqui sua mensagem'/>
-        <button className='button-class'>Enviar</button>
-      </form>
+    <Body>
+      <Form>
+        <Input user=''placeholder='Usuário'/>
+        <Input placeholder='Digite aqui sua mensagem'/>
+        <Button onClick={this.onClickBotao}>Enviar</Button>
+      </Form>
 
-    </div>
+    </Body>
   );
 }
+}
 
-export default App;
+export default Whatslab;
+
