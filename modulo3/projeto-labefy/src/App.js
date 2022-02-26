617b146bfd61b7701e7191f201c3ae5c8a051e16
Laine Moura <lainermoura@gmail.com>
2022-02-26 19:45:07 -0300

    permite que o usuario crie uma playlist com input controlado

--- modulo3/projeto-labefy/src/App.js
+++ modulo3/projeto-labefy/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Home from "./Components/Home"
+export default class App extends React.Component {
+  state={
+    home: true
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    let pagina = <h1>Página Erro</h1>;
+
+      if (this.state.home) {
+        pagina = <Home />;
+      } 
+    return (
+      <div>
+        {pagina}
+      </div>
+    )
+  }
 }
 
-export default App;
