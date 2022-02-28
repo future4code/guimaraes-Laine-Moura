b8b692e89b29dcbc03ee960ae3ec0a91cdcea4a9
Laine Moura <lainermoura@gmail.com>
2022-02-28 17:14:44 -0300

    estiliza pagina de login

--- modulo3/projeto-labefy/src/Components/Login.jsx
+++ modulo3/projeto-labefy/src/Components/Login.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const DivLogin = styled.div`
+  width: 100vw;
+  display: grid;
+  justify-self: center;
+  align-self: center;
+`
+
+const MsgP = styled.p`
+  display: flex;
+  margin: 40vh auto 0;
+  color: #27db27;
+`
+
+const BtnClickLogin = styled.button`
+  display: flex;
+  margin: 5vh auto 0;
+  color: #464646;
+`
+
 
 export default class Login extends React.Component {
   render() {
     return (
-      <div>
-          <button onClick={this.props.onClickLogin}>Login</button>
-      </div>
+      <DivLogin>
+        <MsgP>Bem-vind@ ao Labefy!</MsgP>
+          <BtnClickLogin onClick={this.props.onClickLogin}>Login</BtnClickLogin>
+      </DivLogin>
     )
   }
 }
