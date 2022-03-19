1fb2d273dc94b0cd0cce445fd6ccc6c5f41e64e7
Laine Moura <lainermoura@gmail.com>
2022-03-19 19:06:59 -0300

    configuracao inicial para troca de tela - principal e match

--- modulo4/projeto-astromatch/src/App.js
+++ modulo4/projeto-astromatch/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import TelaPrincipal from './components/TelaPrincipal/TelaPrincipal'
+import TelaMatch from './components/TelaMatch/TelaMatch';
 
-function App() {
-  return (
-    <div>
-     <p>a s t r o m a t c h</p> 
-    </div>
-  );
+const App = () => {
+  const [telaExibida, setTelaExibida] = useState('perfil')
+
+  const escolheTela = () => {
+    switch (telaExibida) {
+      case 'perfil':
+        return <TelaPrincipal onChangeTelaPrincipal={onChangeTelaPrincipal}/>
+      case 'match':
+        return <TelaMatch onChangeTelaMatch={onChangeTelaMatch}/>
+      default:
+        return <p>Oppps!</p> ;
+    }
+  }
+
+  const onChangeTelaPrincipal = () => setTelaExibida('perfil')
+  const onChangeTelaMatch = () => setTelaExibida('match')
+
+
+return (
+  <div>
+   {escolheTela()}
+  </div>
+);
 }
 
 export default App;
--- modulo4/projeto-astromatch/src/components/TelaPrincipal/TelaPrincipal.js
+++ modulo4/projeto-astromatch/src/components/TelaPrincipal/TelaPrincipal.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function TelaPrincipal() {
   return (
-    <div>TelaMatch</div>
+    <div>TelaPrincipal</div>
   )
 }
