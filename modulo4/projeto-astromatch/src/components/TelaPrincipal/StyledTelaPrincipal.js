0dd5cbe1abf8943c96c8421b174321ea8fda1742
Laine Moura <lainermoura@gmail.com>
2022-03-20 12:47:25 -0300

    melhoria de codigo de styledTelaPrincipal - retira propriedades desnecessarias

--- modulo4/projeto-astromatch/src/components/TelaPrincipal/StyledTelaPrincipal.js
+++ modulo4/projeto-astromatch/src/components/TelaPrincipal/StyledTelaPrincipal.js
@@ -2,13 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 export const DivCard = styled.div`
-    margin: 0 auto;
     text-align: center;
-    justify-content: center;
 `
 
 export const DivBotoes = styled.div`
-     margin: 0 auto;
     text-align: center;
-    justify-content: center;
 `
