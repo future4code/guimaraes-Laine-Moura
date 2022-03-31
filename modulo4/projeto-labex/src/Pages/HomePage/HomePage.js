6a92ae8a1935c5600337da41979f7cf93209aa69
Laine Moura <lainermoura@gmail.com>
2022-03-31 11:21:43 -0300

    cria botoes e da funcao a eles

--- modulo4/projeto-labex/src/Pages/HomePage/HomePage.js
+++ modulo4/projeto-labex/src/Pages/HomePage/HomePage.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import {ContainerPrincipal} from './StyledHomePage'
 
 export default function HomePage() {
+
+const navigate = useNavigate()
+const goToLogin = () => navigate('/login')
+const goToListTripPage = () => navigate('listTrips')
+
   return (
     <ContainerPrincipal>
       <p>Home</p>
-      <button >Ver Viagens</button>
-      <button >Área de Admin</button>
+      <button onClick={goToListTripPage}>Ver Viagens</button>
+      <button onClick={goToLogin}>Área de Admin</button>
     </ContainerPrincipal>
   )
 }
