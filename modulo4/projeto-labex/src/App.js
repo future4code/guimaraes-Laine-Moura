8b72eeb77bb78054e1a3f14fda2e9a57ee3ef745
Laine Moura <lainermoura@gmail.com>
2022-04-05 17:12:47 -0300

    adiciona botao de login funcional com a API do LabeX

--- modulo4/projeto-labex/src/App.js
+++ modulo4/projeto-labex/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Router from './Routes/Router';
 
+export const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labeX/laine-moura-guimaraes'
+
 function App() {
   return (
     <Router />
--- modulo4/projeto-labex/src/Pages/LoginPage/LoginPage.js
+++ modulo4/projeto-labex/src/Pages/LoginPage/LoginPage.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
+import { url } from '../../App'
 
 export default function LoginPage() {
 
 const navigate = useNavigate()
 const goToHomePage = () => navigate(-1)
-const goToAdminHomePage =() => navigate('adminHomePage')
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
+
+const handleEmail = (e) => setEmail(e.target.value) 
+const handlePassword = (e) => setPassword(e.target.value) 
+
+const login = () => {
+  const body = {
+    email: email,
+    password: password
+  }
+  axios
+    .post(`${url}/login`, body)
+    .then(res => {
+      localStorage.setItem('token', res.data.token)
+      navigate('adminHomePage')
+    })
+    .catch((err) => err.response)
+}
+
   return (
     <div>
       <p>LoginPage</p>
-      <input type={'email'}/>
-      <input type={'password'}/>
-      <br/>
+        <input type={'email'} placeholder={'Nome'} value={email} onChange={handleEmail}/>
+        <input type={'password'} placeholder={'Senha'} value={password} onChange={handlePassword}/>
+        <br/>
       <br/>
       <button onClick={goToHomePage}>Voltar</button>
-      <button onClick={goToAdminHomePage}>Login</button>
+      <button onClick={login}>Login</button>
 
 
 
