29816a6fe0a46ddc526888a84e91be1f56c11f2a
Laine Moura <lainermoura@gmail.com>
2022-04-07 11:52:58 -0300

    estruturacao inicial da pagina Admin, trazendo as informaçoes de viagens e renderizando na pag

--- modulo4/projeto-labex/src/Pages/AdminHomePage/AdminHomePage.js
+++ modulo4/projeto-labex/src/Pages/AdminHomePage/AdminHomePage.js
@@ -1,22 +1,61 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import useRequestData from '../../Hooks/UseRequestData'
+import axios from 'axios'
+import {url} from '../../App'
+import { TripDiv, BtnDelTrip } from './StyledAdminHomePage'
 
-export default function LoginPage() {
+export default function AdminHomePage() {
 
-const navigate = useNavigate()
-const goToHomePage = () => navigate(-2) /* corrigir */
-const createTrip = () => navigate('createTrip')
+  const [listTrips, setListTrips] = useRequestData(`${url}/trips`)
 
+  const navigate = useNavigate()
+
+
+  const logout = () => localStorage.removeItem('token') 
+  const goToHomePage = () => navigate('/')
+  const createTrip = () => navigate('createTrip')
+  const goToTripDetails = (tripId) => {
+    const tripIdentif = tripId
+      navigate(`/adminHomePage/${tripIdentif}`)
+  } 
+
+  const deleteTrip = (trip) => {
+    const headers = {
+      headers: {
+        auth: localStorage.getItem('token')
+      }
+    }
+
+    if(window.confirm('Deseja deletar a viagem?')) {
+      axios
+      .delete(`${url}/trips/${trip.id}`, headers)
+      .then((res) => navigate('adminHomePage'), setListTrips() )
+      .catch((err) =>  navigate('adminHomePage'))
+    }
+  }
 
   return (
     <div>
-      <p>AdminHomePage</p>
-      <button onClick={goToHomePage}>Voltar</button>
-      <button onClick={createTrip}>Criar Viagem</button>
-      <button onClick={goToHomePage}>Logout</button>
+    {listTrips?.trips.map((trip) => {
+      return (
+        <div onClick={() => goToTripDetails(trip.id)} key={trip.id}>
+            <TripDiv> {trip.name} 
+              <BtnDelTrip onClick={(e) => {
+                e.stopPropagation();
+                deleteTrip(trip)
+              }}> x
+            </BtnDelTrip>
+          </TripDiv>
 
-      <p>viagem aqui</p>
+        </div>
 
-    </div>
-  )
+      )
+    })}
+
+    <button onClick={goToHomePage}>Voltar</button>
+    <button onClick={createTrip}>Criar Viagem</button>
+    <button onClick={logout}>Logout</button>
+  </div>
+);
 }
---
+++ modulo4/projeto-labex/src/Pages/AdminHomePage/StyledAdminHomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from 'styled-components'
+
+export const TripDiv = styled.div `
+    display: flex;
+    margin: 0 auto;
+`
+
+export const BtnDelTrip = styled.button`
+    color: inherit;
+    border: inherit;
+    :hover {
+        color: white;
+        background: grey;
+    }
+`
