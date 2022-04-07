5ffa39c93aebaf05a1759e88c79f6b87814e3344
Laine Moura <lainermoura@gmail.com>
2022-04-07 18:16:18 -0300

    renderiza lista de viagens e estiliza inicialmente a página

--- modulo4/projeto-labex/src/Pages/ListTripsPage/ListTripsPage.js
+++ modulo4/projeto-labex/src/Pages/ListTripsPage/ListTripsPage.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { url } from "../../App";
+import useRequestData from "../../Hooks/UseRequestData";
+import {TitleItem} from './StyledListrips'
 
-export default function ListTripsPage() {
 
-const navigate = useNavigate()
-const goToHomePage = () => navigate(-1)
-const applicationToTrip = () => navigate('applicationToTrip')
+const ListTripsPage = () => {
+
+  const [listTrips] = useRequestData(`${url}/trips`)
+
+  const navigate = useNavigate()
+
+  const goBack = () => {
+    navigate(-1)
+  }
+
+  const goToApplicationFormPage = () => {
+    navigate("/trips/application")
+  }
+
+
+
   return (
+
     <div>
-      <p>ListTripPage</p>
-      
-      <div>
-          <p>Viagem</p>
-      </div>
-
-      <button onClick={goToHomePage}>Voltar</button>
-      <button onClick={applicationToTrip}>Inscrição</button>
-
-
-
+      <h1>Lista De Viagens</h1>
+      {listTrips?.trips.map((trip) => {
+        return (
+          <div>
+            <p key={trip.id}> <TitleItem> Nome: </TitleItem> {trip.name}</p>
+            <p> <TitleItem> Descrição: </TitleItem> {trip.description} </p>
+            <p> <TitleItem> Planeta: </TitleItem> {trip.planet} </p>
+            <p> <TitleItem> DUração em dias:</TitleItem> {trip.durationInDays}</p>
+            <p> <TitleItem> Data:</TitleItem> {trip.date}</p>
+            <br/>
+          </div>
+        )
+      })}
+      <button onClick={goBack}>Voltar</button>
+      <button onClick={goToApplicationFormPage}>Increva-se</button>
     </div>
   )
+
 }
+
+export default ListTripsPage
---
+++ modulo4/projeto-labex/src/Pages/ListTripsPage/StyledListrips.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+export const TitleItem = styled.p`
+    color: gray;
+    display: inline-flex;
+    margin: 0 0;
+    font-weight: 500;
+`
