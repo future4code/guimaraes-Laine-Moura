00dee5c31f41b733b23aede425fec7be8e856a6b
Laine Moura <lainermoura@gmail.com>
2022-05-19 12:18:24 -0300

    resolucao do exercicio 2

--- modulo5/node-package-json/index.js
+++ modulo5/node-package-json/index.js
@@ -6,8 +6,35 @@
 const idade = Number(process.argv[3])
 const idadeMaisSete = idade + 7
 
-console.log(`Olá, ${nome}! Você tem ${idade} anos.`);
+console.log(`EXERCÍCIO 1: Olá, ${nome}! Você tem ${idade} anos.`);
 
 //c
-console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você tera ${idadeMaisSete}.`);
+console.log(`EXERCÍCIO 1: Olá, ${nome}! Você tem ${idade} anos. Em sete anos você tera ${idadeMaisSete}.`);
 }
+
+//Exercício 2
+
+{const operacao = process.argv[2]
+const num1 = Number(process.argv[3])
+const num2 = Number(process.argv[4])
+
+
+switch(operacao){
+	case "soma":
+		total = num1 + num2
+		break;
+	case "subt":
+		total = num1 - num2
+		break;
+    case "mult":
+		total = num1 * num2
+		break;
+    case "div":
+        total = num1 / num2
+        break;
+    default:
+        ("Algo está errado")
+}
+
+console.log(`EXERCÍCIO 2: Resposta: ${total}`);}
+
