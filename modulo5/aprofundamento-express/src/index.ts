09eaa280a16b833fba0dff632f35864ac0aa3058
Laine Moura <lainermoura@gmail.com>
2022-06-02 17:47:27 -0300

    resolucao ex1

---
+++ modulo5/aprofundamento-express/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/", function (req, res){
+    res.send("Página inicial")
+})
+
+app.get("/ping", (req, res) => {          
+    res.send("Pong! 🏓")    
+})
+
+app.listen(3003, () => {
+    console.log('Servidor rodando na url http://localhost:3003');
+})
