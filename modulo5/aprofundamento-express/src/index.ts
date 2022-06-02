bbad54ba55480143d345e5efcd996e1c8a5880db
Laine Moura <lainermoura@gmail.com>
2022-06-02 18:12:56 -0300

    resolucao ex2

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -5,6 +5,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+
 app.get("/", function (req, res){
     res.send("Página inicial")
 })
@@ -12,6 +14,13 @@ app.get("/", function (req, res){
 app.get("/ping", (req, res) => {          
     res.send("Pong! 🏓")    
 })
+
+type toDo = {
+    userId: number,
+    id: number,
+    title:string,
+    completed: boolean
+}
 
 app.listen(3003, () => {
     console.log('Servidor rodando na url http://localhost:3003');
