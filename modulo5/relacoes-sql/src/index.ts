67afaad6f1d712b1244bebc0e6efb034e6b651c7
Laine Moura <lainermoura@gmail.com>
2022-06-23 09:53:41 -0300

    configuracao inicial

--- modulo5/relacoes-sql/src/index.ts
+++ modulo5/relacoes-sql/src/index.ts
@@ -2,6 +2,11 @@ import connection from './connection';
 import app from './app';
 import { Request, Response } from 'express';
 
+/*
+Exercício 1
+A) A chave estrabngeira é a chave capaz de relacionar duas tabelas
+*/
+
 app.get('/test', (req, res) => {
     res.status(200).send("Servidor em pé!");
 });
