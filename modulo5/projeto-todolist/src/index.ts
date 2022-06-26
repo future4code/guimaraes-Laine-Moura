006c69a5b4a6088b0293ba8362dacf1f26d70a9b
Laine Moura <lainermoura@gmail.com>
2022-06-26 16:50:58 -0300

    cria ednpoint - criar usuario

--- modulo5/projeto-todolist/src/index.ts
+++ modulo5/projeto-todolist/src/index.ts
@@ -1,6 +1,33 @@
 import connection from './connection';
 import app from './app';
 import { Request, Response } from 'express';
+import { v4 as generateId } from 'uuid';
+
+const id = generateId()
+
+
+app.post("/user", async (req: Request, res: Response) => {
+    try {
+        const { name, nickname, email } = req.body
+        if( !name || !nickname || !email ) { throw new Error("Parâmetro não enviado");
+         }
+        await connection.raw(`
+        INSERT INTO ToDoListUser (id, name, nickname, email)
+        VALUES(
+            "${generateId()}",
+            "${name}",
+            "${nickname}",
+            "${email}"
+        )
+        `)
+        res.status(201).send(`Usuário ${nickname} adicionado com sucesso!`);
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
+
+})
+
+
 
 app.get('/test', (req, res) => {
     res.status(200).send("Servidor em pé!");
