748124de416271e0914ae3133483499a98acd0e2
Laine Moura <lainermoura@gmail.com>
2022-07-03 13:05:54 -0300

    cria endpoint - cria usuário

---
+++ modulo5/labecommerce/src/endpoints/createUser.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express"
+import app from "../app"
+import { v4 as generateId } from 'uuid';
+import connection from "../connection";
+
+export const createUser = app.post("/users", async (req: Request, res: Response): Promise<void> => {
+    let statusCode = 500;
+    try {
+        const { name, email, password } = req.body
+
+        if (!name || !email || !password) {
+            throw new Error("Não podemos fazer o cadastro ao menos que todos os campos sejam preenchidos. Revise suas informações ;)");
+        }
+
+        const newUser = await connection("labecommerce_users")
+            .insert({
+                id: generateId(),
+                name: name,
+                email: email,
+                password: password
+            })
+            res.status(201).send("Usuário criado com sucesso!")
+
+    } catch (error:any) { res.status(statusCode || 400).send(error.message) }
+})
--- modulo5/labecommerce/src/index.ts
+++ modulo5/labecommerce/src/index.ts
@@ -1,7 +1,10 @@
 import connection from './connection';
 import app from './app';
 import { Request, Response } from 'express';
+// import {createUser} from './endpoints/createUser'
+
+app.post("/users", createUser);
 
 app.get('/test', (req, res) => {
-    res.status(200).send("Servidor em pé!");
+    res.status(200).send("Servidor em pé! 👣");
 });
