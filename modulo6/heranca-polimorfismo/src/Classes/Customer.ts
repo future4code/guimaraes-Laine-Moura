641d97966f74191d91c9bf147452f7c094ce47f1
Laine Moura <lainermoura@gmail.com>
2022-07-17 18:15:45 -0300

    resolucao exercicio 2

---
+++ modulo6/heranca-polimorfismo/src/Classes/Customer.ts
@@ -0,0 +1,40 @@
+import { User } from "./User";
+
+export class Customer extends User {
+    public purchaseTotal: number = 0;
+    private creditCard: string;
+  
+    constructor(
+      id: string,
+      email: string,
+      name: string,
+      password: string,
+      creditCard: string
+    ) {
+      super(id, email, name, password);
+      console.log("Chamando o construtor da classe Customer");
+      this.creditCard = creditCard;
+    }
+  
+    public getCreditCard(): string {
+      return this.creditCard;
+    }
+  }
+
+    /*
+  Exercício 2
+  a) Quantas vezes a mensagem "Chamando o construtor da classe Customer" foi impressa no terminal? 
+  R: Foi impresso uma vez
+
+  b) Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal? Por quê?
+  R: Uma vez porque está imprimindo a frase da classe pai.
+
+  
+  const customer1 = new Customer(
+    "1",
+    "email@email.com",
+    "teste",
+    "123456",
+    "200"
+  );
+  console.log(customer1);*/
--- modulo6/heranca-polimorfismo/src/Classes/User.ts
+++ modulo6/heranca-polimorfismo/src/Classes/User.ts
@@ -1,4 +1,4 @@
-class User {
+export class User {
     private id: string;
     private email: string;
     private name: string;
