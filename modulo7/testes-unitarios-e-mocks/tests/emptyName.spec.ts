363c4ad3a985d334ee3769db0b1fa45b0252a714
Laine Moura <lainermoura@gmail.com>
2022-09-01 17:56:15 -0300

    exercicio 2a - skipteste

--- modulo7/testes-unitarios-e-mocks/tests/emptyName.spec.ts
+++ modulo7/testes-unitarios-e-mocks/tests/emptyName.spec.ts
@@ -1,6 +1,6 @@
 import { validateCharacter } from "../src";
 
-describe("empty name", ()=>{
+describe.skip("empty name", ()=>{
     test("Should return false for empty name", () => {
         const result = validateCharacter({
           name: "",
