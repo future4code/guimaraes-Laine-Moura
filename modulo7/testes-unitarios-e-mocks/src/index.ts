e2f5415a35c9549bef1562f4457beec6cb544b68
Laine Moura <lainermoura@gmail.com>
2022-09-01 17:55:46 -0300

    exercicio 2a - funcionando

--- modulo7/testes-unitarios-e-mocks/src/index.ts
+++ modulo7/testes-unitarios-e-mocks/src/index.ts
@@ -1,6 +1,23 @@
-import { app } from "./controller/app"
-import { Request, Response} from 'express'
+export interface Character {
+  name: string;
+  life: number;
+  strength: number;
+  defense: number;
+}
 
-app.get("/test", (req: Request, res: Response) => {
-    res.status(200).send("Servidor em pé! 👣");
-  });
+export const validateCharacter = (input: Character): boolean => {
+  if (
+    !input.name ||
+		input.life === undefined || 
+    input.strength === undefined ||
+    input.defense === undefined
+  ) {
+    return false;
+  }
+
+  if (input.life <=0 || input.strength <=0 || input.defense <=0) {
+    return false;
+  }
+
+  return true;
+}
